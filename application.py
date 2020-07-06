812f55b0661161dbff454ed2a2373a95a3fd1026
owner <benedikt@kleppmann.de>
2020-07-06 16:55:29 +0200

    this is currently not working. trying a reduced version

--- application.py
+++ application.py
@@ -36,12 +36,24 @@ def simulate():
     error_threshold = request_dict['error_threshold']
 
 
+    #RUN SIMULATION AND CHECK CORRECTNESS
+    y0_values_in_simulation = functions.likelihood_learning_simulator(df, rules, priors_dict, batch_size, is_timeseries_analysis, times, timestep_size, y0_columns, parameter_columns)
+    errors_dict = functions.n_dimensional_distance(y0_values_in_simulation, y0_values, y0_columns, y0_column_dt,error_threshold, rules) 
+
+    simulation_results = {}
+    for rule in rules:
+        if rule['learn_posterior']:
+            simulation_results['nb_of_sim_in_which_rule_' + str(rule['id']) + '_was_used'] = errors_dict[rule['id']]['nb_of_sim_in_which_rule_was_used']
+            simulation_results['error_rule' + str(rule['id'])] = errors_dict[rule['id']]['error'] 
+
+
+
     # SAVE RESULT IN DATABASE 
     connection = psycopg2.connect(user="dbadmin", password="rUWFidoMnk0SulVl4u9C", host="aa1pbfgh471h051.cee9izytbdnd.eu-central-1.rds.amazonaws.com", port="5432", database="postgres")
     cursor = connection.cursor()
     sql_statement = '''INSERT INTO tested_simulation_parameters (simulation_id, run_number, batch_number, priors_dict, simulation_results) VALUES 
                             (%s, %s, %s, '%s', '%s');
-                    ''' % (simulation_id, run_number, batch_number, json.dumps(priors_dict), '{}')
+                    ''' % (simulation_id, run_number, batch_number, json.dumps(priors_dict), json.dumps(simulation_results))
 
     cursor.execute(sql_statement)
 
